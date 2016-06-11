4d702410074641614be72c94f7284075dfdc3200
ahmedamin007 <ahmed_amin007@hotmail.com>
2016-06-11 18:02:18 -0500

    ahmed commit2

---
+++ src/java/com/ebuy/model/Address.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ebuy.model;
+
+import javax.persistence.Entity;
+
+/**
+ *
+ * @author ahmedamin
+ */
+
+@Entity
+public class Address {
+    
+    private String address1;
+    private String address2;
+    
+}
---
+++ src/java/com/ebuy/model/Person.java
@@ -0,0 +1,104 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ebuy.model;
+
+import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+/**
+ *
+ * @author ahmedamin
+ */
+@Entity
+public class Person {
+    
+    @Id @GeneratedValue
+    private int id;
+    private String username;
+    private String password;
+    private String name ;
+    private String phone;
+    private EGender gender;
+    @OneToMany
+    private Address address;
+
+    
+    public Person() {
+    }
+
+    
+    public Person(int id, String username, String password, String name, String phone, EGender gender, Address address) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.name = name;
+        this.phone = phone;
+        this.gender = gender;
+        this.address = address;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public EGender getGender() {
+        return gender;
+    }
+
+    public void setGender(EGender gender) {
+        this.gender = gender;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    
+    
+}
